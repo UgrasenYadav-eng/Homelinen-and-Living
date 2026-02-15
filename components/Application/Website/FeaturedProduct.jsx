4aaa75bddfc90a1f256b250e04beb33c08c0d50a
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2026-02-16 04:33:58 +0530

    update project

--- components/Application/Website/FeaturedProduct.jsx
+++ components/Application/Website/FeaturedProduct.jsx
@@ -3,8 +3,14 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 import ProductBox from "./ProductBox";
 
 const FeaturedProduct = async () => {
+
+  const baseUrl =
+    process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000";
+
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/product/get-featured-product`,
+    `${baseUrl}/api/product/get-featured-product`,
     { cache: "no-store" }
   );
 
