079a912ba809c1da18c94e072d66b57d20efc431
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2026-02-23 09:08:16 +0530

    update project

--- components/Application/Website/FeaturedProduct.jsx
+++ components/Application/Website/FeaturedProduct.jsx
@@ -4,8 +4,11 @@ import ProductBox from "./ProductBox";
 
 const FeaturedProduct = async () => {
 
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
   const res = await fetch(
-    `/api/product/get-featured-product`,
+    `${baseUrl}/api/product/get-featured-product`,
     { cache: "no-store" }
   );
 
@@ -47,6 +50,56 @@ const FeaturedProduct = async () => {
 };
 
 export default FeaturedProduct;
+
+// import Link from "next/link";
+// import { IoIosArrowRoundForward } from "react-icons/io";
+// import ProductBox from "./ProductBox";
+
+// const FeaturedProduct = async () => {
+
+//   const res = await fetch(
+//     `/api/product/get-featured-product`,
+//     { cache: "no-store" }
+//   );
+
+//   if (!res.ok) {
+//     console.error("Failed to fetch featured products");
+//     return null;
+//   }
+
+//   const productData = await res.json();
+
+//   return (
+//     <section className="lg:px-32 px-4 sm:py-10">
+//       <div className="flex justify-between items-center mb-5">
+//         <h2 className="sm:text-4xl text-2xl font-semibold">
+//           Featured Products
+//         </h2>
+
+//         <Link
+//           href="/shop"
+//           className="flex items-center gap-2 underline underline-offset-4 hover:text-primary"
+//         >
+//           View All
+//           <IoIosArrowRoundForward />
+//         </Link>
+//       </div>
+
+//       <div className="grid md:grid-cols-4 grid-cols-2 sm:gap-10 gap-2">
+//         {!productData.success && (
+//           <div className="text-center py-5">Data Not Found.</div>
+//         )}
+
+//         {productData.success &&
+//           productData.data.map((product) => (
+//             <ProductBox key={product._id} product={product} />
+//           ))}
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default FeaturedProduct;
 
 
 
